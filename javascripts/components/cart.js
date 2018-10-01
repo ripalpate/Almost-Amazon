bccc041d9573483bc6c824df87b09c4cb7051c7f
Ripal Patel <ripal212007@gmail.com>
2018-09-30 22:52:09 -0500

    print cart to DOM on click

--- javascripts/components/cart.js
+++ javascripts/components/cart.js
@@ -1,2 +1,11 @@
-import {book} from "../components/store.js"
+import {book} from "../components/store.js";
 
+const getBookDetails = () => {
+    // let newprice= `<h3>${book.title}</h3>`;
+    let newprice =`<br><img src="${book.image}" width="100px" height="100px">`;
+        newprice += `<br><h4>Price: ${book.price}</h4>`;
+ 
+    return newprice;
+ }
+ 
+ export {getBookDetails};
