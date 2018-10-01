8ae9a17b5f782d8f9a9715e18bd31fc4ce141952
Ripal Patel <ripal212007@gmail.com>
2018-10-01 13:03:11 -0500

    added positioning to cart and display cart

--- javascripts/components/cart.js
+++ javascripts/components/cart.js
@@ -2,9 +2,8 @@ import {book} from "../components/store.js";
 
 const getBookDetails = () => {
     // let newprice= `<h3>${book.title}</h3>`;
-    let newprice =`<br><img src="${book.image}" width="100px" height="100px">`;
-        newprice += `<br><h4>Price: ${book.price}</h4>`;
- 
+    let newprice =`<br><img src="${book.image}" width="50px" height="50px">`;
+        newprice += `<br><h6>Price: ${book.price}</h6>`;
     return newprice;
  }
  
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,8 @@
+import {bookBuilder,addToCartButtonEvent} from './components/store.js'
 
 const initializeApp = () =>{
-
+    bookBuilder();
+    addToCartButtonEvent();
 }
-
 
 initializeApp();
